9e8a3ad1858ee01735b932f3d03b002e002ef932
crew-pt <dev@crew.pt>
2017-08-21 08:47:29 +0100

    push

--- index.php
+++ index.php
@@ -6,14 +6,18 @@
     <?php while ( have_posts() ) : the_post();
     ?>
       <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-        <article class="">
-          <header>
-            <h1><?php the_title(); ?></h1>
+        <article class="post-card">
+          <header class="card-header">
+
           </header>
-          <?php
-            the_content();
-            the_date();
-            the_tags(); ?>
+          <section class="card-body">
+            <h3 class="post-title"><?php the_title(); ?></h3>
+            <p class="post-date"><?php the_date(); ?></p>
+            <?php
+              the_content();
+
+              the_tags(); ?>
+          </section>
         </article>
       </div>
     <?php
