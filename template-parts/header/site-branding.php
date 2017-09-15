44781652dd1d74c2fff01cba45c0826e0b9110e8
crew-pt <dev@crew.pt>
2017-09-15 17:45:18 +0100

    updates

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -1,13 +1,12 @@
-<div id="site-branding" class="col-xs-6 col-sm-6 col-md-2 col-lg-2 site-branding">
+<div id="site-branding" class="col-xs-8 col-sm-8 col-md-2 col-lg-2 site-branding">
+  <h1 class="hide-text"><?php bloginfo( 'name' ); ?></h1>
   <a href="<?php echo home_url(); ?>" class="logo">
-    <h1 class="hide-text"><?php bloginfo( 'name' ); ?></h1>
-
     <?php get_template_part( 'template-parts/header/logo', 'svg' ); ?>
   </a>
 </div>
 
 <div id="navigation-toggle">
-  <div class="col-xs-6 col-sm-6">
+  <div class="col-xs-4 col-sm-4">
     <a href="#/" id="toggle-nav" class="toggle-nav" ><i class="fa fa-bars"></i></a>
   </div>
 </div>
