81969ff686bea7ea0d7dd89e160dfcede8c1c0cd
crew-pt <dev@crew.pt>
2017-09-15 15:07:35 +0100

    updates

--- assets/js/header-scroll.js
+++ assets/js/header-scroll.js
@@ -4,8 +4,16 @@
     $(window).on("scroll", function() {
       //console.log('yoyoyo');
       var fromTop = $(window).scrollTop();
+      var vpW = $(window).width();
         if ( $( "body" ).is( ".page-template-page-home" ) ) {
-          $("#main-header").toggleClass("main-header-normal", (fromTop > 100));
+          if ( vpW < 1024 ) {
+            console.log("mais pequeno que 1024");
+            $("#main-header").toggleClass("main-header-normal", (fromTop > 20));
+          }
+          else {
+            console.log("maior que 1024");
+            $("#main-header").toggleClass("main-header-normal", (fromTop > 100));
+          }
         }
     });
   });
