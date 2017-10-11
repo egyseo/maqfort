4bf95d06095ae6a935f18203ba1476e689944f6f
crew-pt <dev@crew.pt>
2017-10-11 16:42:25 +0100

    yo

--- archive-products.php
+++ archive-products.php
@@ -2,7 +2,10 @@
 
 get_header(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <header class="page-header">
+  <header class="page-header-with-img" style="background-color:#525254;background-image:url(<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/img-products.jpg); background-position:bottom; background-repeat:no-repeat; background-size:cover;">
+
+  </header>
+  <section class="page-content">
     <div class="container container-fluid">
       <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
@@ -10,10 +13,6 @@ get_header(); ?>
         </div>
       </div>
     </div>
-  </header>
-  <section class="page-content">
-    <div class="container container-fluid">
-      <div class="row">
         <?php if ( have_posts() ) : ?>
         <section class="page-content">
           <div class="container container-fluid">
@@ -49,7 +48,6 @@ get_header(); ?>
            <?php maqfort_pagination(); ?>
         </div>
       </div>
-    </div>
   </section>
 </article>
 
