f4f36208ad6453477b76a1a7e5687a86a5c48f1c
crew-pt <dev@crew.pt>
2017-10-12 22:44:07 +0100

    added new template

---
+++ page-about.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: About Template
+ * The theme about page template.
+ *
+ * @package AMOB
+ * @since AMOB Theme 1.0
+ *
+ */
+get_header();
+
+while ( have_posts() ) : the_post();
+
+  get_template_part( 'template-parts/pages/about', 'template' );
+
+endwhile; // End of the loop
+
+get_footer();
---
+++ template-parts/pages/about-template.php
@@ -0,0 +1,28 @@
+<?php putRevSlider(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <header class="page-header">
+    <div class="container container-fluid">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <?php the_title( '<h1 class="page-title"><span>', '</span></h1>' ); ?>
+        </div>
+      </div>
+    </div>
+  </header>
+  <section class="page-content">
+    <div class="container container-fluid">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <?php
+            the_content();
+
+            wp_link_pages( array(
+              'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'maqfort' ),
+              'after'  => '</div>',
+            ) );
+          ?>
+        </div>
+      </div>
+    </div>
+  </section>
+</article>
