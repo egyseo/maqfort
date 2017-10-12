68ffc232a8f2bb9fbbe32122c5c65bc7bdba475b
crew-pt <dev@crew.pt>
2017-10-12 21:41:18 +0100

    teste

--- archive-products.php
+++ archive-products.php
@@ -19,7 +19,7 @@ get_header(); ?>
             <div class="row">
               <?php while ( have_posts() ) : the_post(); ?>
 
-                <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
                   <article class="product-card">
                     <header class="product-card-header">
                       <?php if ( has_post_thumbnail() ) : ?>
