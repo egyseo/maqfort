3b94c32da7fe520b484cbc47b6c1883b6e81ec5c
crew-pt <dev@crew.pt>
2017-10-13 00:35:11 +0100

    yeah

--- template-parts/pages/content-contacts.php
+++ template-parts/pages/content-contacts.php
@@ -1,13 +1,13 @@
 <?php
-  $maqfort_address_prefix = '_maqfort_address_';
-  $maqfort_address_title = get_post_meta( get_the_ID(), $maqfort_address_prefix . 'title', true );
-  $maqfort_address_address = get_post_meta( get_the_ID(), $maqfort_address_prefix . 'address', true );
-  $maqfort_address_phone = get_post_meta( get_the_ID(), $maqfort_address_prefix . 'phone', true );
-  $maqfort_address_email = get_post_meta( get_the_ID(), $maqfort_address_prefix . 'email', true );
+  $address_prefix = '_maqfort_address_';
+  $address_title = get_post_meta( get_the_ID(), $address_prefix . 'title', true );
+  $address_address = get_post_meta( get_the_ID(), $address_prefix . 'address', true );
+  $address_phone = get_post_meta( get_the_ID(), $address_prefix . 'phone', true );
+  $address_email = get_post_meta( get_the_ID(), $address_prefix . 'email', true );
   $contact_form_prefix = 'maqfort_contact_form_';
   $contact_form_title = get_post_meta( get_the_ID(), $contact_form_prefix . 'title', true );
   $contact_form_desc = get_post_meta( get_the_ID(), $contact_form_prefix . 'description', true );
-  $contact_form_sc = get_post_meta( get_the_ID(), $contact_form_prefix . 'shortcode', true );
+  $contact_form_shorcode = get_post_meta( get_the_ID(), $contact_form_prefix . 'shortcode', true );
  ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -25,10 +25,18 @@
         </div>
         <div class="row">
           <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-            <h2 class="address-title"><?php echo esc_html( $maqfort_address_title ); ?></h2>
-            <p><?php echo $maqfort_address_address ?></p>
-            <p><?php esc_html_e( 'Tel.', 'maqfort' ); ?> <a href="tel:<?php echo esc_html( $maqfort_address_phone ) ?>"><?php echo esc_html( $maqfort_address_phone ); ?></a></p>
-            <a href="mailto:<?php echo esc_html( $maqfort_address_email ) ?>"><?php _e( 'Send email', 'maqfort' ); ?></a>
+            <?php if ( ! empty( $address_title ) ) {
+              echo '<h2 class="address-title">' . esc_html( $address_title ). '</h2>';
+            }
+            if ( ! empty( $address_address ) ) {
+              echo '<p>' . $address_address . '</p>';
+            }
+            if ( ! empty( $address_phone ) ) {
+              echo '<p>' . esc_html( 'Phone:', 'maqfort' ) . '<a href="tel:' . esc_html( $address_phone ) . '">' . esc_html( $address_phone ) . '</a></p>';
+            }
+            if ( ! empty( $address_email ) ) {
+              echo '<a href="mailto:' . esc_html( $address_email ) . '">' .  __( 'Send email', 'maqfort' ) . '</a>';
+            } ?>
           </div>
           <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
             <p>mapa</p>
@@ -37,9 +45,14 @@
         <section class="contact-form">
           <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              <h2 class="contact-form-title"><?php echo esc_html( $contact_form_title ); ?></h2>
-              <p> <?php echo esc_html( $contact_form_desc ); ?> </p>
-              <?php echo do_shortcode( $contact_form_sc ); ?>
+              <?php if ( ! empty( $contact_form_title ) ) {
+                echo '<h2 class="contact-form-title">' . esc_html( $contact_form_title ) . '</h2>';
+              }
+              if ( ! empty( $contact_form_desc ) ) {
+                echo '<p>' . esc_html( $contact_form_desc ) . '</p>';
+              }
+
+              echo do_shortcode( $contact_form_shorcode ); ?>
             </div>
           </div>
         </section>
@@ -59,10 +72,18 @@
       <div class="container container-fluid">
         <div class="row">
           <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-            <h2 class="address-title"><?php echo esc_html( $maqfort_address_title ); ?></h2>
-            <p><?php echo $maqfort_address_address ?></p>
-            <p><?php esc_html_e( 'Tel.', 'maqfort' ); ?> <a href="tel:<?php echo esc_html( $maqfort_address_phone ) ?>"><?php echo esc_html( $maqfort_address_phone ); ?></a></p>
-            <a href="mailto:<?php echo esc_html( $maqfort_address_email ) ?>"><?php _e( 'Send email', 'maqfort' ); ?></a>
+            <?php if ( ! empty( $address_title ) ) {
+              echo '<h2 class="address-title">' . esc_html( $address_title ). '</h2>';
+            }
+            if ( ! empty( $address_address ) ) {
+              echo '<p>' . $address_address . '</p>';
+            }
+            if ( ! empty( $address_phone ) ) {
+              echo '<p>' . esc_html( 'Phone:', 'maqfort' ) . '<a href="tel:' . esc_html( $address_phone ) . '">' . esc_html( $address_phone ) . '</a></p>';
+            }
+            if ( ! empty( $address_email ) ) {
+              echo '<a href="mailto:' . esc_html( $address_email ) . '">' .  __( 'Send email', 'maqfort' ) . '</a>';
+            } ?>
           </div>
           <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
             <p>mapa</p>
@@ -71,13 +92,17 @@
         <section class="contact-form">
           <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              <h2 class="contact-form-title"><?php echo esc_html( $contact_form_title ); ?></h2>
-              <p> <?php echo esc_html( $contact_form_desc ); ?> </p>
-              <?php echo do_shortcode( $contact_form_sc ); ?>
+              <?php if ( ! empty( $contact_form_title ) ) {
+                echo '<h2 class="contact-form-title">' . esc_html( $contact_form_title ) . '</h2>';
+              }
+              if ( ! empty( $contact_form_desc ) ) {
+                echo '<p>' . esc_html( $contact_form_desc ) . '</p>';
+              }
+
+              echo do_shortcode( $contact_form_shorcode ); ?>
             </div>
           </div>
         </section>
-        </div>
       </div>
     </section>
   <?php } ?>
