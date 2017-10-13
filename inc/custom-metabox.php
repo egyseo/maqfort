cafc8c74137ab80dbf8763b1944097f441a81b1c
crew-pt <dev@crew.pt>
2017-10-13 17:43:25 +0100

    home services update

--- inc/custom-metabox.php
+++ inc/custom-metabox.php
@@ -285,3 +285,86 @@ if ( ! function_exists('maqfort_contact_form') ) {
   add_action( 'cmb2_init', 'maqfort_contact_form' );
 
 }
+
+/*
+* -----------------------------------------------------------
+* 6 Creat metabox for maqfort srevices on home page.
+* -----------------------------------------------------------
+*/
+if ( ! function_exists('maqfort_homepage_services') ) {
+
+  function maqfort_homepage_services() {
+
+  	$prefix = '_maqfort_homepage_';
+
+  	/**
+  	 * Initiate the metabox
+  	 */
+  	$cmb = new_cmb2_box( array(
+  			'id'            => $prefix . 'services',
+  			'title'         => __( 'MAQFORT Services', 'maqfort' ),
+  			'object_types'  => array( 'page', ), // Post type
+        'show_on'      => array( 'key' => 'page-template', 'value' => 'page-home.php' ),
+  			'context'       => 'normal',
+  			'priority'      => 'high',
+  			'show_names'    => true, // Show field names on the left
+  			// 'cmb_styles' => false, // false to disable the CMB stylesheet
+  			// 'closed'     => true, // Keep the metabox closed by default
+  	) );
+    //start group field
+    $group_field_id = $cmb->add_field( array(
+      'id'          => $prefix . 'itens',
+      'type'        => 'group',
+      'description' => __( 'Add Services', 'maqfort' ),
+      // 'repeatable'  => false, // use false if you want non-repeatable group
+      'options'     => array(
+          'group_title'   => __( 'Service {#}', 'maqfort' ), // since version 1.1.4, {#} gets replaced by row number
+          'add_button'    => __( 'Add Service', 'maqfort' ),
+          'remove_button' => __( 'Remover Service', 'maqfort' ),
+          'sortable'      => true, // beta
+          'closed'     => true, // true to have the groups closed by default
+        ),
+    ) );
+
+    // Id's for group's fields only need to be unique for the group. Prefix is not needed.
+    $cmb->add_group_field( $group_field_id, array(
+        'name' => __( 'Service title', 'maqfort' ),
+        'description' => __( 'Insert here the service title.', 'maqfort' ),
+        'id'   => $prefix . 'title',
+        'type' => 'text',
+    ) );
+
+
+    $cmb->add_group_field( $group_field_id, array(
+        'name' => __( 'Service description', 'maqfort' ),
+        'description' => __( 'Insert here the service description.', 'maqfort' ),
+        'id'   => $prefix . 'description',
+        'type' => 'textarea',
+    ) );
+
+    $cmb->add_group_field( $group_field_id, array(
+        'name' => __( 'Button text', 'maqfort' ),
+        'description' => __( 'Insert here the button text.', 'maqfort' ),
+        'id'   => $prefix . 'btn',
+        'type' => 'text',
+    ) );
+
+    $cmb->add_group_field( $group_field_id, array(
+        'name' => __( 'Button URL', 'maqfort' ),
+        'description' => __( 'Insert here the button url.', 'maqfort' ),
+        'id'   => $prefix . 'url',
+        'type' => 'text_url',
+    ) );
+
+    $cmb->add_group_field( $group_field_id, array(
+        'name' => __( 'Service image', 'maqfort' ),
+        'description' => __( 'Insert here the desire image.', 'maqfort' ),
+        'id'   => $prefix . 'image',
+        'type' => 'file',
+    ) );
+
+  }
+
+  add_action( 'cmb2_init', 'maqfort_homepage_services' );
+
+}
--- template-parts/pages/home-services.php
+++ template-parts/pages/home-services.php
@@ -1,21 +1,46 @@
-<section id="home-services"  style="background-attachment:fixed; background-position:center; background-repeat:no-repeat; background-size:cover;">
+<section id="home-services">
   <div class="container container-fluid">
     <div class="row">
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-        <div class="service-box">
-          <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/support.svg" alt="support">
-          <h2 class="section-title">Assistência Técnica</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <button type="button" name="button">Saber mais</button>
-        </div>
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-        <div class="service-box">
-          <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/teaching.svg" alt="support">
-          <h2 class="section-title">Formação</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          <button type="button" name="button">Saber mais</button>
-        </div>
-      </div>
+
+      <?php
+      $service_itens = get_post_meta( get_the_ID(), '_maqfort_homepage_itens', true );
+
+      if ( $service_itens ) {
+
+        foreach ( (array)$service_itens as $key => $service ) {
+
+          $service_title = $service_description = $service_btn = $service_url = $service_image = '';
+
+          if ( isset( $service[ '_maqfort_homepage_title' ] ) )
+            $service_title = esc_html( $service[ '_maqfort_homepage_title' ] );
+
+          if ( isset( $service[ '_maqfort_homepage_description' ] ) )
+            $service_description = esc_html( $service[  '_maqfort_homepage_description' ] );
+
+          if ( isset( $service[ '_maqfort_homepage_btn' ] ) )
+            $service_btn = esc_html( $service[  '_maqfort_homepage_btn' ] );
+
+          if ( isset( $service[ '__maqfort_homepage_url' ] ) )
+            $service_url = esc_html( $service[  '_maqfort_homepage_url' ] );
+
+          if ( isset( $service[ '_maqfort_homepage_image' ] ) )
+            $service_image = wp_get_attachment_url( $service[ '_maqfort_homepage_image_id' ] );
+
+          if ( !empty( array( $service_title ) ) ) { ?>
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+              <div class="service-box">
+                <img src="<?php echo $service_image ?>">
+                <h2 class="service-title"><?php  echo $service_title ?></h2>
+                <p><?php echo $service_description ?></p>
+                <a href="<?php echo $service_url ?>" target="_self" class="service-btn"><?php echo $service_btn ?></a>
+              </div>
+            </div>
+          <?php }
+
+        }
+
+      }
+      ?>
     </div>
+  </div>
 </section>
