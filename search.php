0428daaedb43c1e53d87e9c5880e82aa696bf382
crew-pt <dev@crew.pt>
2018-01-16 01:26:33 +0000

    last changes to backup

--- search.php
+++ search.php
@@ -1,29 +1,46 @@
 <?php get_header(); ?>
 
-<section class="search-page">
+<article id="post-<?php the_ID(); ?> post-content" <?php post_class(); ?>>
   <?php if ( have_posts() ) : ?>
+  <header class="search-header">
     <div class="container container-fluid">
       <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
           <h1 class="page-title">
-            <?php echo esc_html_e( 'Search results for', 'maqfort' ) ?>:  <span><?php the_search_query(); ?></span>
+            <?php echo esc_html_e( 'Search results for', 'maqfort' ) ?>: <span><?php the_search_query(); ?></span>
           </h1>
         </div>
       </div>
-      <div class="row">
-        <?php while ( have_posts() ) : the_post(); ?>
-          <?php do_action( 'maqfort_loop' ); ?>
-        <?php endwhile; // End of the loop
-        else:
-          get_template_part( 'template-parts/post/content', 'none' );
-        endif; ?>
-        </div><!-- row ends -->
-        <div class="row">
-          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-             <?php maqfort_pagination(); ?>
-          </div>
-        </div>
     </div>
-</section>
+  </header>
+  <section class="search-page">
+    <div class="container container-fluid">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+          <?php
+            while ( have_posts() ) : the_post();
+
+              do_action( 'maqfort_loop' );
+
+            endwhile;
+
+            else:
+
+              get_template_part( 'template-parts/post/content', 'none' );
+
+            endif;
+          ?>
+          
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+           <?php maqfort_pagination(); ?>
+        </div>
+      </div>
+    </div>
+  </section>
+</article>
 
 <?php get_footer(); ?>
--- template-parts/post/content-none.php
+++ template-parts/post/content-none.php
@@ -1,31 +1,30 @@
+<div class="container container-fluid">
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <header>
+        <h1><?php esc_html_e( 'Nothing Found!', 'maqfort' ); ?></h1>
+      </header>
+      <section>
+        <?php
+          if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-	<div class="container container-fluid">
-		<div class="row">
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<header>
-					<h1><?php esc_html_e( 'Nothing Found!', 'maqfort' ); ?></h1>
-				</header>
-        <section>
-          <?php
-            if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+            <p><?php printf( wp_kses( __( 'Ready to write your first post? <a href="%1$s">Start here</a>.', 'maqfort' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
-              <p><?php printf( wp_kses( __( 'Ready to write your first post? <a href="%1$s">Start here</a>.', 'maqfort' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+          <?php elseif ( is_search() ) : ?>
 
-            <?php elseif ( is_search() ) : ?>
+            <p><?php esc_html_e( 'Sorry,  but we cant find anything with your search terms. Please try again with new terms.', 'maqfort' ); ?></p>
+            <?php
+            get_search_form();
 
-              <p><?php esc_html_e( 'Sorry,  but we cant find anything with your search terms. Please try again with new terms.', 'maqfort' ); ?></p>
-              <?php
-                get_search_form();
+          else : ?>
 
-            else : ?>
+            <p><?php esc_html_e( 'We cant find what you are looking for, prehaps search can help you.', 'maqfort' ); ?></p>
+            <?php
+            get_search_form();
 
-              <p><?php esc_html_e( 'We cant find what you are looking for, prehaps search can help you.', 'maqfort' ); ?></p>
-              <?php
-                get_search_form();
-
-            endif;
-          ?>
-        </section>
-			</div>
-		</div>
-	</div>
+          endif;
+        ?>
+      </section>
+    </div>
+  </div>
+</div>
