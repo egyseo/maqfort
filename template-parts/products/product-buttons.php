6dfcf37676b52e102955d4c434a257ef70b7d3f5
crew-pt <dev@crew.pt>
2019-11-10 12:25:44 +0000

    somefixes

--- template-parts/products/product-buttons.php
+++ template-parts/products/product-buttons.php
@@ -13,7 +13,7 @@ $more_info = get_theme_mod( 'mf_product_form' );
 <div class="product-buttons">
 	<?php
 	if ( $more_info ) :
-		echo '<a class="btn btn-primary" data-fancybox data-src="#hidden-content" href="javascript:;" ><i class="fa fa-info" aria-hidden="true"></i>' , esc_html__( 'Mais informações', 'maqfort' ) , '</a>';
+		echo '<a class="btn btn-primary" data-fancybox data-src="#hidden-content" href="javascript:;" ><i class="fa fa-info" aria-hidden="true"></i>' , esc_html__( 'Peça mais informações', 'maqfort' ) , '</a>';
 	endif;
 	?>
 	<button class="btn btn-print" onclick="printPage()"><i class="fa fa-print" aria-hidden="true"></i><?php esc_html_e( 'Imprimir este produto', 'maqfort' ); ?></button>
