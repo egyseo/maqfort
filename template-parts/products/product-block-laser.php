e501272525f0364be94ddd6cdb456a75cd9532a9
crew-pt <dev@crew.pt>
2019-11-12 23:51:54 +0000

    css fixes on tabs

--- template-parts/products/product-block-laser.php
+++ template-parts/products/product-block-laser.php
@@ -13,7 +13,9 @@ $product_block_laser_description = get_theme_mod( 'mf_product_block_laser' );
 ?>
 
 <section id="laser-block" class="product-block">
-	<h4 class="block-title"><?php esc_html_e( 'Os lasers NUKON', 'maqfort' ); ?></h4>
+		<div class="section-title-wrapper">
+			<h4 class="section-title"><span><?php esc_html_e( 'Os lasers NUKON', 'maqfort' ); ?></span></h4>
+		</div>
 	<div class="laser-block-content">
 		<img src="<?php echo esc_url( $product_block_laser_image ); ?>" alt="laser-nukon" />
 		<div class="block-description">
